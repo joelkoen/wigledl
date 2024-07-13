7342039a91433e67b7473b406e62f62bf183ebe6
Joel Koen <mail@joelkoen.com>
2024-07-13 16:15:27 +1000

    feat: pagination
    (untested)

--- src/main.rs
+++ src/main.rs
@@ -23,29 +23,46 @@ struct Transaction {
 fn main() -> Result<()> {
     let cli = Cli::parse();
     let auth = format!("basic {}", cli.token);
-
     let agent = Agent::new();
-    let transactions: Transactions = agent
-        .get("https://api.wigle.net/api/v2/file/transactions")
-        .set("authorization", &auth)
-        .call()?
-        .into_json()?;
 
-    println!("Found {} uploads", transactions.results.len());
-    for x in transactions.results {
-        let id = x.transid;
-        let file = PathBuf::from(format!("{id}.csv"));
-        if file.exists() {
-            continue;
-        }
-
-        println!("Downloading {id}");
-        let data = agent
-            .get(&format!("https://api.wigle.net/api/v2/file/csv/{id}"))
+    let mut page = 0;
+    loop {
+        let transactions: Transactions = agent
+            .get(&format!(
+                "https://api.wigle.net/api/v2/file/transactions?pagestart={}&pageend={}",
+                page * 100,
+                (page + 1) * 100
+            ))
             .set("authorization", &auth)
             .call()?
-            .into_string()?;
-        fs::write(file, data)?;
+            .into_json()?;
+        let count = transactions.results.len();
+        println!(
+            "Found {} uploads on page {page}",
+            transactions.results.len()
+        );
+
+        for x in transactions.results {
+            let id = x.transid;
+            let file = PathBuf::from(format!("{id}.csv"));
+            if file.exists() {
+                continue;
+            }
+
+            println!("Downloading {id}");
+            let data = agent
+                .get(&format!("https://api.wigle.net/api/v2/file/csv/{id}"))
+                .set("authorization", &auth)
+                .call()?
+                .into_string()?;
+            fs::write(file, data)?;
+        }
+
+        if count == 100 {
+            page += 1;
+        } else {
+            break;
+        }
     }
 
     Ok(())
